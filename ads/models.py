80a929e6e7577c85c5924298982055513a0032be
MichaelRodionov <106465054+MichaelRodionov@users.noreply.github.com>
2023-02-02 20:30:47 +0300

    add slug field

--- ads/models.py
+++ ads/models.py
@@ -1,16 +1,25 @@
 from django.db.models import Model, IntegerField, CharField, BooleanField
+from django.forms import SlugField
 
 
 # ----------------------------------------------------------------
 # models
 class Advertisement(Model):
-    name = CharField(max_length=300)
+    slug = SlugField(max_length=50)
+    name = CharField(max_length=400)
     author = CharField(max_length=30)
     price = IntegerField()
     description = CharField(max_length=1000)
     address = CharField(max_length=500)
     is_published = BooleanField()
 
+    def __str__(self):
+        return self.slug
+
 
 class Category(Model):
+    slug = SlugField(max_length=50)
     name = CharField(max_length=100)
+
+    def __str__(self):
+        return self.slug
