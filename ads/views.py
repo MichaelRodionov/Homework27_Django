4fbc030e3cffd32f7b30be5d30012ce63a96a612
MichaelRodionov <106465054+MichaelRodionov@users.noreply.github.com>
2023-02-02 23:41:59 +0300

    fix views

--- ads/views.py
+++ ads/views.py
@@ -18,42 +18,39 @@ def check_response(request) -> JsonResponse:
     :param request: request
     :return: JsonResponse
     """
-    if request.method == 'GET':
-        # fill_database()
-        return JsonResponse({'status': 'OK'}, status=200)
+    # fill_database()
+    return JsonResponse({'status': 'OK'}, status=200)
 
 
 # ----------------------------------------------------------------
 # CBV
 @method_decorator(csrf_exempt, name='dispatch')
 class CategoryView(View):
-    @staticmethod
-    def get(request) -> JsonResponse:
+    def get(self, request) -> JsonResponse:
         """
         View to handle GET request to show categories
         :param request: request
         :return: JsonResponse
         """
-        if request.method == 'GET':
-            categories = Category.objects.all()
-            return JsonResponse([{
-                'id': category.id,
-                'name': category.name
-            } for category in categories], safe=False, status=200)
+        categories = Category.objects.all()
+        return JsonResponse([self._create_response(category) for category in categories], safe=False, status=200)
 
-    @staticmethod
-    def post(request) -> JsonResponse:
+    def post(self, request) -> JsonResponse:
         """
         View to handle POST request to create new category
         :param request: request
         :return: JsonResponse
         """
-        category_data = json.loads(request.body)
-        category = Category.objects.create(name=category_data['name'])
-        return JsonResponse({
-            'id': category.id,
-            'name': category.name
-        }, safe=False, status=201)
+        try:
+            category_data = json.loads(request.body)
+            category = Category.objects.create(name=category_data['name'])
+        except Exception:
+            return JsonResponse({'error': 'Invalid request'}, status=400)
+        return JsonResponse(self._create_response(category), status=201)
+
+    @staticmethod
+    def _create_response(category: Category) -> dict:
+        return {'id': category.pk, 'name': category.name}
 
 
 class CategoryDetailView(DetailView):
@@ -78,43 +75,39 @@ class CategoryDetailView(DetailView):
 
 @method_decorator(csrf_exempt, name='dispatch')
 class AdvertisementView(View):
-    @staticmethod
-    def get(request) -> JsonResponse:
+    def get(self, request) -> JsonResponse:
         """
         View to handle GET request to show advertisements
         :param request: request
         :return: JsonResponse
         """
-        if request.method == 'GET':
-            advertisements = Advertisement.objects.all()
-            return JsonResponse([{
-                'id': advertisement.id,
-                'name': advertisement.name,
-                'author': advertisement.author,
-                'price': advertisement.price,
-                'description': advertisement.description,
-                'address': advertisement.address,
-                'is_published': advertisement.is_published,
-            } for advertisement in advertisements], safe=False, status=200)
+        advertisements = Advertisement.objects.all()
+        return JsonResponse([self._create_response(advertisement) for advertisement in advertisements], safe=False, status=200)
 
-    @staticmethod
-    def post(request) -> JsonResponse:
+    def post(self, request) -> JsonResponse:
         """
         View to handle GET request to create new advertisement
         :param request: request
         :return: JsonResponse
         """
-        advertisement_data = json.loads(request.body)
-        advertisement = Advertisement.objects.create(**advertisement_data)
-        return JsonResponse({
-            'id': advertisement.id,
+        try:
+            advertisement_data = json.loads(request.body)
+            advertisement = Advertisement.objects.create(name=advertisement_data['name'])
+        except Exception:
+            return JsonResponse({'error': 'Invalid request'}, status=400)
+        return JsonResponse(self._create_response(advertisement), status=201)
+
+    @staticmethod
+    def _create_response(advertisement: Advertisement) -> dict:
+        return {
+            'id': advertisement.pk,
             'name': advertisement.name,
             'author': advertisement.author,
             'price': advertisement.price,
             'description': advertisement.description,
             'address': advertisement.address,
-            'is_published': advertisement.is_published,
-        }, safe=False, status=201)
+            'is_published': advertisement.is_published
+        }
 
 
 class AdvertisementDetailView(DetailView):
